7dad273b0b2e71edfcfc32c40ad9d43c4c90e645
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-12-18 15:49:35 +0700

    menambahkan destinasi home

---
+++ app/src/main/java/com/example/questapi_077/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.questapi_077.uicontroller.route
+
+import com.example.questapi_077.R
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route= "home"
+    override val titleRes = R.string.app_name
+
+}
