05116e6292e4146a92174a1acd7b2e4a81426b22
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-12-18 12:50:44 +0700

    menambahkan service Retrofit untuk mengambil data insert dan data siswa

---
+++ app/src/main/java/com/example/questapi_077/apiservice/ServiceApiSiswa.kt
@@ -0,0 +1,15 @@
+package com.example.questapi_077.apiservice
+
+import com.example.questapi_077.modeldata.DataSiswa
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface ServiceApiSiswa {
+    @GET("bacaTeman.php")
+    suspend fun getSiswa(): List<DataSiswa>
+
+    @POST("insertTM.php")
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa): Response<Void>
+}
