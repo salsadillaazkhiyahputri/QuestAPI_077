29364167e0a91d6faf9aa38c11f0de8f0542ebd4
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-12-18 16:02:25 +0700

    mengubaah namanya

---
+++ app/src/main/java/com/example/questapi_077/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.questapi_077
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.questapi_077.ui.theme.QuestAPI_077Theme
+import com.example.questapi_077.uicontroller.DataSiswaApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            QuestAPI_077Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    DataSiswaApp(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
