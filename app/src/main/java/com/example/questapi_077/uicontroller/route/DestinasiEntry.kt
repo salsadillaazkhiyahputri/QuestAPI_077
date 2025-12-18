c95b9d9046ad55f7e6001f75f13e42a315d46805
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-12-18 15:49:47 +0700

    menambahkan destinasi entry

---
+++ app/src/main/java/com/example/questapi_077/uicontroller/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.questapi_077.uicontroller.route
+
+import com.example.questapi_077.R
+
+object DestinasiEntry : _root_ide_package_.com.example.questapi_077.uicontroller.route.DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
