f52f4ad94a845830f4f07b112701bfe878501c6b
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-12-25 12:28:34 +0700

    lengkapi fungsi operasional data siswa di ServiceApiSiswa

--- app/src/main/java/com/example/questapi_077/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_077/repositori/RepositoryDataSiswa.kt
@@ -7,6 +7,9 @@ import retrofit2.Response
 interface RepositoryDataSiswa{
     suspend fun getDataSiswa():List<DataSiswa>
     suspend fun  postDataSiswa(dataSiswa: DataSiswa) : retrofit2.Response<Void>
+    suspend fun getSatuSiswa(id:Int) : DataSiswa
+    suspend fun editSatuSiswa(id:Int,dataSiswa: DataSiswa):retrofit2.Response<Void>
+    suspend fun hapusSatuSiswa(id:Int):retrofit2.Response<Void>
 
 }
 
