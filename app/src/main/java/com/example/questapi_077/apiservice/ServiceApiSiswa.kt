dfd8c3a7d66c74afd7aaaaecb94bd9f7f6465e9d
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-12-25 11:36:43 +0700

    tambah interface ServiceApiSiswa untuk integrasi API

--- app/src/main/java/com/example/questapi_077/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_077/apiservice/ServiceApiSiswa.kt
@@ -3,13 +3,25 @@ package com.example.questapi_077.apiservice
 import com.example.questapi_077.modeldata.DataSiswa
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
 
 interface ServiceApiSiswa {
     @GET("bacaTeman.php")
     suspend fun getSiswa(): List<DataSiswa>
 
     @POST("insertTM.php")
-    suspend fun postSiswa(@Body dataSiswa: DataSiswa): Response<Void>
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
+
+    @GET("bacaTeman.php/{id}")
+    suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
+
+    @PUT("editTM.php/{id}")
+    suspend fun editSatuSiswa(@Query("id") id: Int,@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
+
+    @DELETE("deleteTM.php/{id}")
+    suspend fun hapusSatuSiswa(@Query("id") id: Int):retrofit2.Response<Void>
 }
