cc6acbd61d763a672d117efdb5c6c1c725ec4ab9
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-12-25 12:31:14 +0700

    lengkapi fungsi operasional data siswa di ServiceApiSiswa

--- app/src/main/java/com/example/questapi_077/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_077/repositori/RepositoryDataSiswa.kt
@@ -19,4 +19,9 @@ class JaringanRepositoryDataSiswa(
 ): RepositoryDataSiswa{
     override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
     override suspend fun postDataSiswa(dataSiswa: DataSiswa): Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+    override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
+    override suspend fun editSatuSiswa(id: Int,dataSiswa: DataSiswa):retrofit2.Response<Void> =
+        serviceApiSiswa.editSatuSiswa(id, dataSiswa)
+    override suspend fun hapusSatuSiswa(id: Int):retrofit2.Response<Void> = serviceApiSiswa
+        .hapusSatuSiswa(id)
 }
